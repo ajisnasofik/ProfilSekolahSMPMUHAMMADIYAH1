3876bdce5532e370eb445b10898a54525ef1f18e
JasmineAR985 <jasminramadhani.223@gmail.com>
2021-02-04 13:57:57 +0700

    jasmine

--- app/src/main/java/com/example/profilsekolahsmpmuhammadiyah1/MainActivity.java
+++ app/src/main/java/com/example/profilsekolahsmpmuhammadiyah1/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.profilsekolahsmpmuhammadiyah1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void profil(View view){
+        Intent intent=new Intent(MainActivity.this, ProfilActivity.class);
+        startActivity(intent);
+    }
+    public void prestasi(View view){
+        Intent intent=new Intent(MainActivity.this, PrestasiActivity.class);
+        startActivity(intent);
+    }
+    public void absensi(View view){
+        Intent intent=new Intent(MainActivity.this, AbsensiActivity.class);
+        startActivity(intent);
+    }
+    public void tugas(View view){
+        Intent intent=new Intent(MainActivity.this, TugasActivity.class);
+        startActivity(intent);
+    }
+    public void ekstrakulikuler(View view){
+        Intent intent=new Intent(MainActivity.this, EkstrakulikulerActivity.class);
+        startActivity(intent);
+    }
+
+    public void informasi(View view){
+        Intent intent=new Intent(MainActivity.this, InformasiActivity.class);
+        startActivity(intent);
     }
 }
