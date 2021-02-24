fb9f96613699caa6cca57538920f559c58bcb488
JasmineAR985 <mrehan13@gmail.com>
2021-02-24 14:14:02 +0700

    v8

--- app/src/main/java/com/example/profilsekolahsmpmuhammadiyah1/AbsensiActivity.java
+++ app/src/main/java/com/example/profilsekolahsmpmuhammadiyah1/AbsensiActivity.java
@@ -9,6 +9,6 @@ public class AbsensiActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_absensi);
+        setContentView(R.layout.activity_profil_guru);
     }
 }
